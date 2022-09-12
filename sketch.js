a4e66a8a6bca48094159c6cff619722e0c3e57ce
Paczkiiiii <113376214+Paczkiiiii@users.noreply.github.com>
2022-09-12 11:53:38 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var s1
+var s2
+function setup() 
+{
+  createCanvas(400, 400);
+  s1 = new Student("john",12,6);
+  s2 = new Student("andrew",12,7);
+  s1.display()
+  s2.display()
+}
+function draw() 
+{
+  background(51);
+ 
+}
+
+
